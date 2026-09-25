73ebbe3d0c09da4ff059fe7fcdc353bceeb6530c
prajwal <prajwalnaganoor6@gmail.com>
2026-09-26 05:25:57 +0530

    add the app file and connection to the mysql database

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "CREDIT_CARD_PAYMENTS",
+        "USER":"root",
+        "PASSWORD":"Prajwal@123",
+        "HOST":"localhost",
+        "PORT":"3306"
     }
 }
 
