6074b3ebe7d1d48f99c5e3cbc2622d287d8b42b5
prajwal <prajwalnaganoor6@gmail.com>
2026-09-26 05:53:22 +0530

    modified the admin pennal

--- myproject/users/admin.py
+++ myproject/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User,Cards
 # Register your models here.
+
+class user(admin.ModelAdmin):
+    list_display=["email"]
+admin.site.register(User,user)
+class card(admin.ModelAdmin):
+    list_display = ["user","card_number","expiry_data","card_holder_name"]
+admin.site.register(Cards,card)
