ee13b93318ca4c100e43625b294f54e83955e2b2
prajwal <prajwalnaganoor6@gmail.com>
2026-09-26 07:21:00 +0530

    worked on login logic

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 import json
+import jwt
+from datetime import datetime,timezone,timedelta
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password,check_password
 from  django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
@@ -59,3 +62,66 @@ def register(request):
             {"error":"Invalid JSON"},
             status = 400
         )
+@csrf_exempt
+def login(request):
+    if request.method != 'POST':
+        return JsonResponse(
+            {"error":"Only POST method are allowed here"},
+            status = 405
+        )
+    try:
+        data = json.loads(request.body)
+        username = data.get("username")
+        password = data.get("password")
+
+        if not username or not password:
+            return JsonResponse(
+                {"error":"Username and Password required"},
+                status = 400
+            )
+        try:
+            user = User.objects.get(username=username)
+        except:
+            return JsonResponse(
+                {"error":"User not exists"},
+                status = 400
+            )
+        if not check_password(password,user.password):
+            return JsonResponse(
+                {"error":"Invalid username or password"},
+                status = 400
+            )
+        plyload = {
+            "user_id":user.id,
+            "username":user.username,
+            "first_name":user.first_name,
+            "last_name":user.last_name
+        }
+
+        token = jwt.encode(
+            plyload,
+            settings.SECRET_KEY,
+            algorithm="HS256"
+
+        )
+
+        return JsonResponse(
+            {"message":"Login Successfully",
+            "access token":token,
+            "token type":"Bearer",
+            "User":{
+                "User id":user.id,
+                "username":user.username,
+                "Email":user.email
+            }
+            }
+
+        )
+    except:
+        return json.JSONDecodeError(
+            {
+                "error":"Invalid JSON"
+            },
+            status = 400
+        )
+
